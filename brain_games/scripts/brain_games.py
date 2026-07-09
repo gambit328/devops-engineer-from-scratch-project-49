2a6323c0b0fcc23ab50edfaf5705dfd4bd5abb07
gambit328 <73571911+gambit328@users.noreply.github.com>
2026-07-09 17:51:34 +0700

    add greeting in brain_games

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,10 @@
+import prompt
+
+
 def main():
     print("Welcome to the Brain Games!")
+    name_user = prompt.string("May I have your name? ")
+    print("Hello, " + name_user)
 
 
 if __name__ == "__main__":
