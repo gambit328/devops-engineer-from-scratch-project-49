f516310d38519c2e0ee9f6dbaafcd1fa19bd9d3d
gambit328 <73571911+gambit328@users.noreply.github.com>
2026-07-09 18:01:11 +0700

    change concatination string on f-string in print welcome_user

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,4 +4,4 @@ import prompt
 def welcome_user():
     print("Welcome to the Brain Games!")
     name_user = prompt.string("May I have your name? ")
-    print("Hello, " + name_user)
+    print(f"Hello, {name_user}!")
