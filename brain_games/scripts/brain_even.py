9fabf4b3f579bd564157a767ffa3844ce0a87aff
gambit328 <73571911+gambit328@users.noreply.github.com>
2026-07-12 17:18:52 +0700

    rewrite script brain_even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,17 +1,9 @@
-from brain_games.brain_even_game import (
-    quest_generator,
-    question_answer_and_result,
-    get_string_for_end,
-)
-from brain_games.cli import welcome_user
+from brain_games.games.brain_even_game import get_questions, get_task
+from brain_games.game_engine import game_run
 
 
 def main():
-    user_name = welcome_user()
-    questions = quest_generator()
-    print("'yes' if the number is even, otherwise answer 'no'.")
-    flag = question_answer_and_result(questions)
-    print(get_string_for_end(user_name, flag))
+    game_run(get_questions, get_task)
 
 
 if __name__ == "__main__":
