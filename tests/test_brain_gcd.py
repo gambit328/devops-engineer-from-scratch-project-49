903731e739b542efb82448bdd6c7fc04644ea27e
gambit328 <73571911+gambit328@users.noreply.github.com>
2026-07-12 22:19:42 +0700

    add tests for brain_gcd

---
+++ tests/test_brain_gcd.py
@@ -0,0 +1,24 @@
+import brain_games
+from brain_games.games.brain_gcd_game import (
+    get_questions,
+    get_task,
+    generate_question,
+)
+
+
+def test_get_questions(monkeypatch):
+    numbers = iter([25, 50, 100, 52, 3, 9])
+
+    monkeypatch.setattr(
+        "brain_games.games.brain_gcd_game.randint", lambda a, b: next(numbers)
+    )
+
+    assert get_questions() == [("50 25", "25"), ("100 52", "4"), ("9 3", "3")]
+
+
+def test_get_task():
+    assert get_task() == "Find the greatest common divisor of given numbers"
+
+
+def test_generate_question():
+    assert generate_question(50, 25) == ("50 25", "25")
