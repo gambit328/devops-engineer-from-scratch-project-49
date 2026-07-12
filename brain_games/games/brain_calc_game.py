231b57e5e8c3d277fc60c7a9e5d9e0e1d5abf35a
gambit328 <73571911+gambit328@users.noreply.github.com>
2026-07-12 19:24:20 +0700

    add brain_calc_game + script brain_calc

---
+++ brain_games/games/brain_calc_game.py
@@ -0,0 +1,37 @@
+from random import randint
+
+
+def get_questions():
+    operations = ["+", "-", "*"]
+    questions = []
+
+    for _ in range(3):
+        num1 = randint(0, 50)
+        num2 = randint(0, 50)
+        operation = operations[randint(0, 2)]
+
+        question, correct_answer = generate_question(num1, num2, operation)
+
+        questions.append((question, correct_answer))
+
+    return questions
+
+
+def get_task():
+    return "What is the result of the expression?"
+
+
+def generate_question(num1, num2, operation):
+    correct_answer = 0
+
+    match operation:
+        case "+":
+            correct_answer += num1 + num2
+        case "-":
+            correct_answer += num1 - num2
+        case "*":
+            correct_answer += num1 * num2
+
+    question = f"{num1} {operation} {num2}"
+
+    return (question, correct_answer)
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,10 @@
+from brain_games.game_engine import game_run
+from brain_games.games.brain_calc_game import get_questions, get_task
+
+
+def main():
+    game_run(get_questions, get_task)
+
+
+if __name__ == "__main__":
+    main()
